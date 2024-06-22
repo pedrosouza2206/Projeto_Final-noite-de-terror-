2fb0e8b3d32d6584dc261a9b75e8472e357f4e8a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-21 22:48:25 -0300

    pronto

---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿
+
+using System.Net;
+using Figgle;
+
+Console.Clear();
+string[] tipos =
+{
+   "O Zumbi",
+   "O Assassino",
+   "O Psicopata",
+   "O Palhaço",
+   "O Carniceiro",
+   "O Matador",
+   "O Vampiro",
+   "O Maluco",
+   "O Vingador",
+   "O Monstro",
+   "O Bruxo",
+   "O Demonio"
+};
+string[] variedades =
+{
+   "Endiabrado",
+   "Radioativo",
+   "Endemoniado",
+   "Vermelho",
+   "Possuido",
+   "Macabro",
+   "Sem cabeca",
+   "Cadeirante",
+   "Sem olhos",
+   "Oculto",
+   "Caido",
+   "Amaldiçado",
+   "Invocado",
+   "Desconhecido",
+   "Esquecido",
+   "Costurado",
+   "Anormal",
+   "Lugebre",
+   "Tenebroso",
+   "Imortal",
+   "Burro",
+   "Do inferno",
+   "Abandonado",
+   "Desconhecido",
+   "Do cemiterio",
+   "Sem lugar",
+   "Da lua cheia",
+   "Do velho poço",
+   "Do porão",
+   "Sanguinario",
+   "Pertubado",
+};
+ 
+string tipo, variedade;
+ 
+int mes, dia;
+ 
+ 
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(":::::Noite de Terror:::::");
+Console.ResetColor();
+ 
+ 
+Console.Write("Qual o dia do aniversario da vitima?(1 até 31):");
+dia = Convert.ToInt32(Console.ReadLine());
+if (dia < 1 || dia > 31)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Esse dia não é compatível");
+    Console.ResetColor();
+    return;
+}
+ 
+Console.Write("Qual o mes do aniversario da vitima?(1 até 12):");
+mes = Convert.ToInt32(Console.ReadLine());
+if (mes < 1 || mes > 12)
+
+{  
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Esse mês não é compatível");
+    Console.ResetColor();
+    return;
+}
+
+if (mes<8&&dia>30&&mes%2==0)
+{Console.WriteLine("Dia não bate com mes!");
+return;}
+else if(mes==2&&dia>29)
+{Console.WriteLine("Dia não bate com mes!");
+return;}
+if (mes>7&&dia>30&&mes%2==1)
+{Console.WriteLine("Dia não bate com mes!");
+return;}
+ 
+tipo = tipos[mes - 1];
+variedade = variedades[dia - 1];
+ Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write($"{tipo} {variedade}");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\n\n"+FiggleFonts.Big.Render("Obrigado."));
+Console.ResetColor();
+
+
--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# Projeto_Final-noite-de-terror-
-Meu projeto final de programação 
+# Noite de terrorcl
+Em uma noite de terror, os monstros buscam suas vítimas após ouvir o chamado das estrelas.
+
+Descubra qual o mostro que perseguirá a vítima de acordo com uma data de nascimento e a tabela abaixo.
+
+![downloads](/imgees/download.jpg)
+
+### Nome - Mês de nascimento
+
+1. Janeiro - O Zumbi
+
+2. Fevereiro -	O Assassino
+
+3. Março -	O Psicopata
+
+4. Abril -	O Palhaço
+
+5. Maio -	O Carniceiro
+
+6. Junho -	O Matador
+
+7. Julho -	O Vampiro
+
+8. bAgosto - O Maluco
+
+9. Setembro -	O Vingador
+
+10. Outubro	- O Monstro
+
+11. Novembro  -	O Bruxo
+
+12. Dezembro  -	O Demônio
+
+### Sobrenome - Dia do nascimento
+
+1.	Endiabrado
+
+2.	Radioativo
+
+3.	Endemoniado
+
+4.	Vermelho
+
+5.	Possuído
+
+6.	Macabro
+
+7.	Sombrio
+
+8.	Sem Cabeça
+
+9.	Desconhecido
+
+10.	Inconsciente
+
+11.	Oculto
+
+12.	Esquecido
+
+13.	Lúgubre
+
+14.	Invocado
+
+15.	Caído
+
+16.	Costurado
+
+17.	Amaldiçoado
+
+18.	Anormal
+
+19.	Perturbado
+
+20.	Sanguinário
+
+21.	Tenebroso
+
+22.	Imortal
+
+23.	Ressuscitado
+
+24.	do Pântano
+
+25.	da Encruzilhada
+
+26.	do Porão
+
+27.	do Velho Poço
+
+28.	sem Perdão
+
+29.	do Cemitério
+
+30.	da Meia-Noite
+
+31.	da Lua Cheia
+
+# "Como faço para executar o programa?"
+
+primeiramente, é necessário [baixar o programa](net8.0.rar)
+
+
+
